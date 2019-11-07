b41641ddbbba95f107973574c42e2738b7fbb84f
Ana Muñoz <munoz.am.118@gmail.com>
2019-11-07 12:30:40 -0500

    test classes added

--- test/model/BalanceGeneralTest.java
+++ test/model/BalanceGeneralTest.java
@@ -1,13 +1,38 @@
 package model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class BalanceGeneralTest {
+	
+	private BalanceGeneral bg;
+	
+	private void setupScenary1() {
+		
+	}
+	
+	private void setupScenary2() {
+		bg = new BalanceGeneral("Atlas Ltda.");
+	}
 
 	@Test
-	void test() {
+	public void balanceGeneralTest() {
+		setupScenary1();
+		String empresa = "Atlas Ltda.";
+		bg = new BalanceGeneral(empresa);
+		assertNotNull("la empresa no fue creada", bg != null);
+		
+	}
+
+	@Test
+	public void sumarActivosCorrientesTest() {
+		setupScenary2();
+		assertNotNull("The list is null", bg.getActivos()!=null);
+		assertNotNull("The list is null", bg.getGastos()!=null);
+		assertNotNull("The list is null", bg.getPatrimonio()!=null);
+		assertNotNull("The list is null", bg.getPasivos()!=null);
+		assertNotNull("The list is null", bg.getIngresos()!=null);
 		
 	}
 
